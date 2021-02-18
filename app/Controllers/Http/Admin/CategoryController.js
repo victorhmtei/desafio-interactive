17518415f6d1afb059c88a6c2c3109fed80d9320
Victor Teixeira <vhmteixeira@gmail.com>
2021-02-18 03:44:53 -0300

    controller category

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Category = use('App/Models/Category')
+
 /**
  * Resourceful controller for interacting with categories
  */
@@ -16,8 +18,18 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * @param {Object} ctx.pagination
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, view, pagination }) {
+
+    const title = request.input('title')
+    const query = Category.query()
+    if(title){
+      query.where('title', 'LIKE',`%${title}%`)
+    }
+    
+    const categories = await query.paginate(pagination.page, pagination.limit)
+    return response.send(categories)
   }
 
    /**
@@ -29,6 +41,20 @@ class CategoryController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+
+    try{
+      const {title, description, image_id} = request.all()
+      const category = await Category.create({title, description, image_id})
+      return response.status(201).send(category)
+    }catch (error){
+
+      return response.status(400).send({
+        message: "Error ao processar sua solicitação"
+      })
+
+    }
+
+    
   }
 
   /**
@@ -40,8 +66,14 @@ class CategoryController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params:{id}, request, response }) {
+
+    const category = await Category.findOrFail(id)
+    return response.send(category)
+
+
   }
+
 
   
   /**
@@ -52,7 +84,12 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params:{id}, request, response }) {
+    const categoy = await Category.findOrFail(id)
+    const { title, description, image_id} = request.all()
+    category.merge({title,description,image_id})
+    await category.save()
+    return response.send(category)
   }
 
   /**
@@ -63,7 +100,12 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: {id}, request, response }) {
+
+    const category = await Category.findOrFail(id)
+    await category.delete()
+    return response.status(204).send()
+
   }
 }
 
