6c62d277206e545ff69aa3ac56591cf85b44a874
Victor Teixeira <vhmteixeira@gmail.com>
2021-02-19 08:50:12 -0300

    bug fixed

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -143,11 +143,8 @@ class ImageController {
     try {
       let filepath = Helpers.publicPath(`uploads/${image.path}`)
 
-      await fs.unlink(filepath, err =>{
-        if(!err)
-        await image.delete()
-      })
-
+      fs.unlinkSync(filepath)      
+      await image.delete()  
       return response.status(204).send()
     } catch (error) {
       return response.status(400).send({
