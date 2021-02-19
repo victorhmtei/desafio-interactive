5d8b5fe2a035d5120f1fad0bffc2bff0f6452d15
Victor Teixeira <vhmteixeira@gmail.com>
2021-02-19 06:02:04 -0300

    imagem controller

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const { formatDates } = require('../../../Models/PasswordReset')
+
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Image = use('App/Models/Image')
+const {manage_single_upload, manage_multiple_uploads} = use('App/Helpers')
+const fs = use('fs')
 
 /**
  * Resourceful controller for interacting with images
@@ -17,19 +23,9 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new image.
-   * GET images/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index ({ response, pagination }) {
+    const images = await Image.query().orderBy('id', 'DESC').paginate(pagination.page, pagination.limit)
+    return response.send(images)
   }
 
   /**
@@ -41,6 +37,60 @@ class ImageController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    try {
+      // captura uma image ou mais do request
+      const fileJar = request.file('images',{
+        types: [ 'image'],
+        size: '2mb'
+      })
+
+      // retorno pro usuario
+
+      let images = []
+      // caso seja um unico arquivo - manage_single_upload
+      
+
+      if(!fileJar.files){
+        const file = await manage_single_upload(fileJar)
+        if(file.moved()){
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype 
+          })
+
+          images.push(image)
+
+          return response.status(201).send({successes: images, errors:{}})
+        }
+
+        return response.status(400).send({
+          message: 'Não foi possivel processar esta imagem no momento!'
+        })
+      }
+      // caso seja varios arquivos manage_multiple_uploads
+
+      let files = await manage_multiple_uploads(fileJar)
+
+      await Promise.all(
+        files.successes.map(async file =>{
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+          images.push(image)
+        })
+      )
+
+      return response.status(201).send({successes: images, errors: files.error})
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possivel processar a sua solicitação!'
+      })
+    }
   }
 
   /**
@@ -53,20 +103,11 @@ class ImageController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
+    const image = await Image.findOrFail(id)
+    return response.send(image)
   }
 
-  /**
-   * Render a form to update an existing image.
-   * GET images/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
+  
   /**
    * Update image details.
    * PUT or PATCH images/:id
@@ -75,7 +116,18 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params:{id}, request, response }) {
+    const image = await Image.findOrFail(id)
+    try {
+      image.merge(request.only(['original_name']))
+      await image.save()
+      response.status(200).send(image)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possivel atualizar a imagem'
+      })
+      
+    }
   }
 
   /**
@@ -86,7 +138,23 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params:{id}, request, response }) {
+    const image = await Image.findOrFail(id)
+    try {
+      let filepath = Helpers.publicPath(`uploads/${image.path}`)
+
+      await fs.unlink(filepath, err =>{
+        if(!err)
+        await image.delete()
+      })
+
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possivel deletar a imagem no momento!'
+      })
+      
+    }
   }
 }
 
--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -26,8 +26,72 @@ const Helpers = use('Helpers')
 
  }
 
+ /**
+  * Move um arquivo para um caminho especificado, se nenhum for especificado entao 'public/uploads sera utilizado
+  * 
+  * @param { FileJar} file o arquivo a ser gerenciado
+  * @param {string} path o caminho apra onde o arquivo deve ser movido
+  * @return {Object<FileJar>}
+  * 
+  */
+
+  const manage_single_upload = async ( file, path = null) => {
+      path = path ? path : Helpers.publicPath('uploads')
+      const random_name = await str_random(30)
+      let filename = `${new Date().getTime()} - ${random_name}.${file.subtype}`
+
+      //renomeia o arquivo e move ele para o path
+
+      await file.move(path, {
+          name: filename
+      })
+
+      return file
+
+  }
+
+  /**
+   * Move multiplos arquivos para um caminho especificado, se nenhum for especificado entao 'public/uploads sera utilizado
+   * @param {FileJar} fileJar
+   * @param { string} path 
+   * @return { Object}
+   * 
+   */
+
+   const manage_multiple_uploads = async ( fileJar, path = null) =>{
+       path = path ? path : Helpers.publicPath('uploads')
+
+       let successes = [],
+       errors = []
+
+       await Promise.all(fileJar.files.map(async file =>{
+           let random_name = await str_random(30)
+           let filename = `${new Date().getTime()} - ${random_name}.${file.subtype}`
+
+           //move o arquivo
+
+           await file.move(path, {
+               name: filename
+           })
+
+           //verifca se moveu mesmo
+
+           if(file.moved()){
+               successes.push(file)
+           } else{
+               errors.push(file.error())
+           }
+       })
+       )
+
+       return { successes, errors}
+   }
+
  module.exports ={
-     str_random
+     str_random,
+     manage_single_upload,
+     manage_multiple_uploads
+
  }
 
 
